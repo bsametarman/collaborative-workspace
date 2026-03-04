22accdb6f435bc0be47df159851dd2b4847d98c6
bsametarman <bsametarman@outlook.com>
2026-03-04 12:13:05 +0300

    gateway-service first commit

---
+++ gateway-service/src/main/java/com/collaborativeworkspace/workspace_gateway/WorkspaceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.collaborativeworkspace.workspace_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceGatewayApplication.class, args);
+	}
+
+}
