7cc20f0fd0a71d43d25464584eef8088a1c561e5
bsametarman <bsametarman@outlook.com>
2026-03-05 17:59:57 +0300

    feat: add worker

---
+++ snapshot-worker/src/main/java/com/collaborativeworkspace/snapshot_worker/SnapshotWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.collaborativeworkspace.snapshot_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshotWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapshotWorkerApplication.class, args);
+	}
+
+}
---
+++ snapshot-worker/src/test/java/com/collaborativeworkspace/snapshot_worker/SnapshotWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collaborativeworkspace.snapshot_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapshotWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
