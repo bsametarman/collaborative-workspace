93fa08fa0a7f29930a9eb4896e0f46741cc1472f
bsametarman <bsametarman@outlook.com>
2026-03-06 20:25:14 +0300

    feat: implement event sourcing snapshot engine and api

--- snapshot-worker/src/main/java/com/collaborativeworkspace/snapshot_worker/SnapshotWorkerApplication.java
+++ snapshot-worker/src/main/java/com/collaborativeworkspace/snapshot_worker/SnapshotWorkerApplication.java
@@ -2,8 +2,10 @@ package com.collaborativeworkspace.snapshot_worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SnapshotWorkerApplication {
 
 	public static void main(String[] args) {
