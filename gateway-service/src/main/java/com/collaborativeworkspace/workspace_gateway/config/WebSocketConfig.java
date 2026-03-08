11d56acdc5799e0ac0e5e7f5a40dea2ef062622c
bsametarman <bsametarman@outlook.com>
2026-03-08 14:27:14 +0300

    feat: add jwt validator, auth interceptor to read jwt

--- gateway-service/src/main/java/com/collaborativeworkspace/workspace_gateway/config/WebSocketConfig.java
+++ gateway-service/src/main/java/com/collaborativeworkspace/workspace_gateway/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.collaborativeworkspace.workspace_gateway.config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+	
+	public WebSocketConfig(WebSocketAuthInterceptor webSocketAuthInterceptor) {
+		this.webSocketAuthInterceptor = webSocketAuthInterceptor;
+	}
+	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/topic");
@@ -20,5 +27,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		registry.addEndpoint("/ws")
 			.setAllowedOriginPatterns("*")
 			.withSockJS();
+	}
+	
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+	    registration.interceptors(webSocketAuthInterceptor);
 	}
 }
